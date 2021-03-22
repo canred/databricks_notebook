26ff047b4a8e1ded714b6140824a7e10ea371146
陳慧鴻 <canred.chen@gmail.com>
2021-03-23 02:33:46 +0800

    Update: Quickstart Notebook.sql notebooks test.py test.sql.sql test2.sql

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 # Databricks notebook source
-print("Canred Testing1")
-print("Canred Testing2")
-print("Canred Testing3")
-print("Canred Testing4")
-print("Canred Testing4")
+print("Canred Testing")
+print("Canred Testing")
